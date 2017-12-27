a5fccfdab08ddc0741485a49dff2911e936b23d4
Paweł Dudczyk <dudczyk@o2.pl>
2017-12-27 17:01:33 +0100

    1st

---
+++ SWay/Helpers/APIGoogle.cs
@@ -0,0 +1,38 @@
+﻿using Google.Maps;
+using Google.Maps.Geocoding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWay.Helpers
+{
+    public static class APIGoogle
+    {
+        public static void Register(string apiKey)
+        {
+            GoogleSigned.AssignAllServices(new GoogleSigned(apiKey));
+        }
+
+        public static LatLng GetLatLng(string address)
+        {
+            var response = GeocodingRequest(address);
+            var result = response.Results.First();
+
+            LatLng ret = new LatLng(result.Geometry.Location.Latitude, result.Geometry.Location.Longitude);
+
+            return ret;
+        }
+
+        private static GeocodeResponse GeocodingRequest(string address)
+        {
+            var req = new GeocodingRequest();
+            req.Address = address;
+            req.Sensor = false;
+            var response = new GeocodingService().GetResponse(req);
+
+            return response;
+        }
+    }
+}
--- SWay/Program.cs
+++ SWay/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Google.Maps;
+using Google.Maps.DistanceMatrix;
+using SWay.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,43 @@ namespace SWay
     {
         static void Main(string[] args)
         {
+            List<string> addresses = new List<string>();
+            addresses.Add("34-620 Jodłownik, Jodłownik 44/7");
+            addresses.Add("43-150 Bieruń, Homera 30/32");
+            addresses.Add("Kraków, Bojki 24");
+            addresses.Add("Wrocław, Sztabowa 10");
+            addresses.Add("Kasina Wielka 421");
+
+            APIGoogle.Register("AIzaSyALT_bBzigIMao0ri0hm7kjhMuiNKmUJYU");
+
+            List<LatLng> listLatLng = new List<LatLng>();
+            foreach (string item in addresses)
+            {
+                listLatLng.Add(APIGoogle.GetLatLng(item));
+            }
+
+            DistanceMatrixRequest request = new DistanceMatrixRequest()
+            {
+                WaypointsOrigin = new List<Location>(listLatLng),
+                WaypointsDestination = new List<Location>(listLatLng),
+                Sensor = false
+            };
+
+            var response = new DistanceMatrixService().GetResponse(request);
+
+            for (int i = 0; i < response.Rows.Count(); i++)
+            {
+
+                for (int j = 0; j < response.Rows[i].Elements.Count(); j++)
+                {
+                    Console.WriteLine(response.DestinationAddresses[i] + "  <->  " + response.DestinationAddresses[j]);
+                    Console.WriteLine(response.Rows[i].Elements[j].distance.Text);
+                    Console.WriteLine(response.Rows[i].Elements[j].duration.Text);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
